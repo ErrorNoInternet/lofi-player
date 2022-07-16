d59a8ef6cc24687864a6d0027c535d162e4235ac
ErrorNoInternet <error.nointernet@gmail.com>
2022-07-16 16:19:46 +0800

    Improve playback

--- main.go
+++ main.go
@@ -2,55 +2,43 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"math/rand"
 	"os"
 	"os/exec"
 	"os/signal"
 	"strconv"
-	"time"
 
 	"github.com/sacOO7/gowebsocket"
 )
 
-var streaming bool
-var streamPath string = "/tmp/lofi-stream.ts"
+var (
+	streaming  bool = true
+	streamPipe io.Writer
+	received   int
+)
 
 func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 	sessionId := strconv.Itoa(rand.Intn(2147483648))
-	environmentPath := os.Getenv("TEMP")
-	if environmentPath != "" {
-		streamPath = environmentPath + "\\lofi-stream.ts"
-	}
-	ioutil.WriteFile(streamPath, []byte(""), 0644)
-	streamFile, errorObject := os.OpenFile(streamPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 6400)
-	if errorObject != nil {
-		fmt.Println("Unable to open stream file")
-		return
-	}
-	defer streamFile.Close()
 
 	socket := gowebsocket.New("ws://lofi-server.herokuapp.com/" + sessionId)
 	socket.OnConnected = func(socket gowebsocket.Socket) {
 		fmt.Println("Successfully connected to server")
-		if !streaming {
-			go playAudio()
-		}
+		go playAudio()
 	}
-	socket.OnDisconnected = func(_ error, socket gowebsocket.Socket) {
+	socket.OnDisconnected = func(err error, socket gowebsocket.Socket) {
 		if streaming {
-			fmt.Println("Disconnected from server")
-			ioutil.WriteFile(streamPath, []byte(""), 0644)
+			fmt.Println("Disconnected from server: " + err.Error())
 			socket.Connect()
 		}
 	}
 	socket.OnBinaryMessage = func(data []byte, socket gowebsocket.Socket) {
-		streaming = true
-		fmt.Println("Receiving stream data...")
-		streamFile.WriteString(string(data))
+		fmt.Printf("\r[%v] Receiving stream data...", received)
+		io.WriteString(streamPipe, string(data))
 		socket.SendText("pong")
+		received += 1
 	}
 	socket.Connect()
 
@@ -60,20 +48,18 @@ func main() {
 			fmt.Println("Stopping...")
 			streaming = false
 			socket.Close()
-			os.Exit(0)
 			return
 		}
 	}
 }
 
 func playAudio() {
-	for !streaming {
-		time.Sleep(1 * time.Second)
-	}
 	for {
-		exec.Command("mpv", "--no-video", streamPath).Output()
+		command := exec.Command("mpv", "--no-video", "-")
+		streamPipe, _ = command.StdinPipe()
+		command.Run()
 		if !streaming {
-			os.Exit(0)
+			return
 		}
 		fmt.Println("Restarting audio player...")
 	}
